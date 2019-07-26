3fc4f417c2928e19ac322944530306378e7eaefd
ahmedshindy <53304706+ahmedshindy@users.noreply.github.com>
2019-07-27 01:58:46 +0200

    destructors

--- main.cpp
+++ main.cpp
@@ -1,50 +1,31 @@
- // deriv_VirtualFunctions.cpp
-// compile with: /EHsc
-// microsoft example "real world not just an example: 
-
 #include <iostream>
-using namespace std;
-
-class Account {
+using namespace std; 
+class A
+{
+    int id;
+    static int count;
 public:
-   Account( double d ) { _balance = d; }
-    double GetBalance() { return _balance; }
-   virtual void PrintBalance() { cerr << "Error. Balance not available for base type." << endl; }
-private:
-    double _balance;          // only accout class can access it.
+    A() {
+        count++;
+        id = count;
+        cout << "constructor for id " << id << endl;
+    }
+    ~A() {
+        cout << "destructor for id " << id << endl;
+    }
 };
-
-
-
-class CheckingAccount : public Account {
-public:
-   CheckingAccount(double d) : Account(d) {}             // initializes the previus constructor awesome 
-   void PrintBalance() { cout << "Checking account balance: " << GetBalance() << endl; }
-};
-
-
-
-class SavingsAccount : public Account {
-public:
-   SavingsAccount(double d) : Account(d) {}
-   void PrintBalance() { cout << "Savings account balance: " << GetBalance(); }
-};
-
-
-
-
-
-
+  
+int A::count = 0; 
+  void func () {
+       A ob;
+    ob.~A();
+    
+    cout<<"all is well in func "<<endl;  
+  }
+  
 int main() {
-   // Create objects of type CheckingAccount and SavingsAccount.
-   CheckingAccount *pChecking = new CheckingAccount( 100.00 ); 
-   SavingsAccount  *pSavings  = new SavingsAccount( 1000.00 );
+   func();
+       cout<<"all is well in main\n the constructor gets "<<
+       "called by default even if you called it  "<<endl;  
 
-   // Call PrintBalance using a pointer to Account.
-   Account *pAccount = pChecking;
-   pAccount->PrintBalance();
-
-   // Call PrintBalance using a pointer to Account.
-   pAccount = pSavings;
-   pAccount->PrintBalance();
 }
